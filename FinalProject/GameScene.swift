cc49da744381d147f44c4c966ce311d66a90c269
Victoria <victoria-m@users.noreply.github.com>
2017-12-15 14:35:39 -0600

    First commit: in my game the sprite is able to move around (animated) and I have a foreground and background

--- FinalProject/GameScene.swift
+++ FinalProject/GameScene.swift
@@ -1,87 +1,166 @@
-//
-//  GameScene.swift
-//  FinalProject
-//
-//  Created by Victoria on 12/15/17.
-//  Copyright © 2017 Victoria. All rights reserved.
-//
-
 import SpriteKit
 import GameplayKit
 
+// reference: https://www.raywenderlich.com/89222/sprite-kit-animations-texture-atlases-swift
+
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var rudolph: SKSpriteNode!
+    var rudolphWalkingFrames: [SKTexture]!
+    var background = SKSpriteNode(imageNamed: "background")
+    var foreground = SKSpriteNode(imageNamed: "foreground")
     
     override func didMove(to view: SKView) {
+        // center the background and add it to the scene
+        background.position = CGPoint(x: frame.size.width / 2, y: frame.size.height / 2)
+        background.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        background.size.height = self.size.height
+        background.size.width = self.size.width
+        addChild(background)
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        // set the foreground's position to the bottom of the screen and add it to the scene
+        foreground.position = CGPoint(x: self.frame.midX, y: self.frame.maxY)
+        foreground.size.width = self.size.width
+        addChild(foreground)
+
+        // set up the rudolph walking frames
+        let rudolphAnimatedAtlas = SKTextureAtlas(named: "rudolph")
+        var walkFrames = [SKTexture]()
+        
+        let numImages = rudolphAnimatedAtlas.textureNames.count
+        
+        // add each image from the rudolph atlas folder to the walkframes array
+        for index in 1 ... numImages {
+            let rudolphTextureName = "rudolph\(index)"
+            walkFrames.append(rudolphAnimatedAtlas.textureNamed(rudolphTextureName))
         }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        rudolphWalkingFrames = walkFrames
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        // get the first walking frame and position it in the center of the screen
+        let firstFrame = rudolphWalkingFrames[0]
+        rudolph = SKSpriteNode(texture: firstFrame)
+        
+        rudolph.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
+        rudolph.xScale = 2
+        rudolph.yScale = 2
+        addChild(rudolph)
+        
+        walkingRudolph()
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+    func walkingRudolph() {
+        // this is the walk action method for the rudolph sprite
+        rudolph.run(SKAction.repeatForever(
+            SKAction.animate(with: rudolphWalkingFrames,
+                                         timePerFrame: 0.2,
+                                         resize: false,
+                                         restore: true)),
+                       withKey:"walkingInPlaceRudolph")
     }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
+
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    func rudolphMoveEnded() {
+        rudolph.removeAllActions()
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        // 1
+        let touch = touches.first as! UITouch
+        let location = touch.location(in: self)
+        var multiplierForDirection: CGFloat
+        
+        // 2
+        let rudolphVelocity = self.frame.size.width / 3.0
+        
+        // 3
+        let moveDifference = CGPoint(x: location.x - rudolph.position.x, y: location.y - rudolph.position.y)
+        let distanceToMove = sqrt(moveDifference.x * moveDifference.x + moveDifference.y * moveDifference.y)
+        
+        // 4
+        let moveDuration = distanceToMove / rudolphVelocity
+        
+        // 5
+        if moveDifference.x < 0 { multiplierForDirection = 1.0 }
+        else { multiplierForDirection = -1.0 }
+        
+        rudolph.xScale = -1 * fabs(rudolph.xScale) * multiplierForDirection
+        
+        // 1
+        if (rudolph.action(forKey: "rudolphMoving") != nil) {
+            //stop just the moving to a new location, but leave the walking legs movement running
+            rudolph.removeAction(forKey: "rudolphMoving")
+        }
+        
+        // 2
+        if (rudolph.action(forKey: "walkingInPlaceRudolph") == nil) {
+            //if legs are not moving go ahead and start them
+            walkingRudolph()
+        }
+        
+        // 3
+        let moveAction = (SKAction.move(to: location, duration:(Double(moveDuration))))
+        
+        // 4
+        let doneAction = (SKAction.run({
+            print("Animation Completed")
+            self.rudolphMoveEnded()
+        }))
+        
+        // 5
+        let moveActionWithDone = (SKAction.sequence([moveAction, doneAction]))
+        rudolph.run(moveActionWithDone, withKey:"rudolphMoving")
     }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
+
+//
+//    func touchDown(atPoint pos : CGPoint) {
+//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+//            n.position = pos
+//            n.strokeColor = SKColor.green
+//            self.addChild(n)
+//        }
+//    }
+//
+//    func touchMoved(toPoint pos : CGPoint) {
+//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+//            n.position = pos
+//            n.strokeColor = SKColor.blue
+//            self.addChild(n)
+//        }
+//    }
+//
+//    func touchUp(atPoint pos : CGPoint) {
+//        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+//            n.position = pos
+//            n.strokeColor = SKColor.red
+//            self.addChild(n)
+//        }
+//    }
+//
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        if let label = self.label {
+//            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+//        }
+//
+//        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+//    }
+//
+//    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+//    }
+//
+//    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+//    }
+//
+//    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+//    }
+//
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
